46d20be0bf68ed73c77171aa0ce77247b414a09b
Keli06001 <keli06001@utb.vaxjo.se>
2023-10-11 10:14:58 +0200

    Add project files.

---
+++ Uppgift3.4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace uppgift3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många minuter är låten? Skriv i heltal och avrunda ner");
+            int minuter=int.Parse(Console.ReadLine());
+            if (minuter >= 2 && minuter <= 4)
+            {
+                Console.WriteLine("Hur många sekunder avrundade du ner? Skriv i heltal");
+                int sekunder = int.Parse(Console.ReadLine());
+                if (minuter == 2 && sekunder>=45)
+                {
+                    Console.WriteLine("Din låt får spelas");
+                }
+                else if (minuter == 3 && sekunder >= 0)
+                {
+                    Console.WriteLine("Din låt får spelas");
+                }
+                else if (minuter == 4 && sekunder <= 20) 
+                {
+                    Console.WriteLine("Din låt får spelas");
+                }
+                else
+                {
+                    Console.WriteLine("Din låt får inte spelas");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Din låt får inte spelas");
+            }
+            
+        }
+    }
+
+}
